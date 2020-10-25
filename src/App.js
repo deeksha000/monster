0eff1b941878177dd033583478a9c18bacfdcd6f
Deeksha R <r000deeksha@gmail.com>
2020-10-26 03:15:50 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React,{Component} from 'react';
 import  { CardList }  from './components/card-list/card-list.component';
 import './App.css';
+import {SearchBox} from './components/search-box/search-box.components.jsx';
 
 class App extends Component{
   constructor()
   {
     super();
     this.state={
-      monsters : []
+      monsters : [],
+      searchField : ''
     };
+
   }
+handlechange = (e) => {
+  this.setState({ searchField : e.target.value})
+}
+
   componentDidMount(){
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json() )
     .then(users => this.setState({monsters : users}));
   }
-render(){  return (
+render(){
+  const { monsters, searchField} = this.state;
+  const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
+  return (
     <div className="App">
-    <input type='search' placeholder='search monster'/>
-    <CardList monsters={this.state.monsters} />
+    <h1>MONSTERS ROLODEX</h1>
+    <SearchBox
+    placeholder='search monster'
+    handlechange = { this.handlechange }
+    />
+
+    <CardList monsters={filteredMonsters} />
     </div>
   );
 }
